26ccdce8dccf648f00a7ec4a3e30667afa32481f
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-06-04 01:49:35 +0800

    create wheather details components

---
+++ components/WeatherDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function WeatherDetails() {
+    return (
+        <View>
+            <Text>Weather details</Text>
+        </View>
+    )
+}
+
