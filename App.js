42918be0c0a7edc641ace6052f5306c9664bac88
Debashis Ray <debaray14@gmail.com>
2021-06-04 16:07:09 +0600

    check credentials

--- App.js
+++ App.js
@@ -6,25 +6,40 @@ import { WEATHER_API_KEY } from 'react-native-dotenv';
 const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 
 export default function App() {
-
   const [currentWeather, setCurrentWeather] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [unitsSystem, setUnitsSystem] = useState('metric')
 
   useEffect(() => {
     load()
   }, [])
+
   async function load() {
     setCurrentWeather(null);
-    try{
-      let {status} = await Location.requestForegroundPermissionsAsync()
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync()
 
-      if(status !== 'granted'){
+      if (status !== 'granted') {
         setErrorMessage('Access to location is needed to run the app.');
         return;
       }
       const location = await Location.getCurrentPositionAsync();
-      const {latitude,longitude} = location.coords;
-      alert(latitude,longitude);
+      const { latitude, longitude } = location.coords;
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+
+      const response = await fetch(weatherUrl)
+
+      const result = await response.json()
+
+      if (response.ok) {
+        setCurrentWeather(result)
+        alert(result);
+      } else {
+        setErrorMessage(result.message)
+        alert(result.message);
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
     }
   }
   return (
