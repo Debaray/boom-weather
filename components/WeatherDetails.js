e610c0468c99f63fd1ecd9cd345329b77153044f
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-06-04 12:16:14 +0800

    added temeperature section

--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -11,9 +11,17 @@ export default function WeatherDetails({ currentWeather, unitsSystem }) {
     } = currentWeather;
     const windSpeed = unitsSystem === 'metric' ? `${Math.round(speed)} m/s` : `${Math.round(speed)} miles/h`;
     return (
-        <View>
-            <Text>Weather details</Text>
-        </View>
+        <View style={styles.weatherDetails}>
+            <View style={styles.weatherDetailsRow}>
+                <View style={{ ...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR }}>
+                    <View style={styles.weatherDetailsRow}>
+                        <FontAwesome5 name="temperature-low" size={25} color={PRIMARY_COLOR} />
+                        <View style={styles.weatherDetailsItems}>
+                            <Text>Feels like :</Text>
+                            <Text style={styles.textSecondary}>{feels_like} °</Text>
+                        </View>
+                    </View>
+                </View>
     )
 }
 
