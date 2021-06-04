00232c9768c9e18c90f448f646b38bf685d0ca3f
unknown <ashikrobi@gmail.com>
2021-06-04 09:46:36 +0600

    Created primary structure

--- components/ReloadIcon.js
+++ components/ReloadIcon.js
@@ -1,10 +1,25 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {View, Platform, StyleSheet} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function ReloadIcon() {
+  const reloadIconName = Platform.OS === "ios" ? "ios-refresh" : "md-refresh";
   return (
-    <View>
-      <Text>This is Reload Icon</Text>
+    <View style={styles.reloadIcon}>
+      <Ionicons
+        onPress={ } //add load function after it is created
+        name={reloadIconName }
+        size={24}
+        color={} //add primary color
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  reloadIcon: {
+    position: "absolute",
+    top: 30,
+    right: 20,
+  },
+});
