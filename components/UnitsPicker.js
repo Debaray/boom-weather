8ed9bcfca6243f21c35feac91f2e0533c6d0608d
unknown <ashikrobi@gmail.com>
2021-06-04 10:35:53 +0600

    added imports and style for weather detail

--- components/UnitsPicker.js
+++ components/UnitsPicker.js
@@ -1,10 +1,23 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
+import {colors} from "../Utils/brandColors";
+import {FontAwesome5, MaterialCommunityIcons} from "@expo/vector-icons";
+const {PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR} = colors;
 
 export default function UnitsPicker() {
   return (
-    <View>
+    <View style={styles.weatherDetails}>
       <Text>This is units picker</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  weatherDetails: {
+    marginTop: "auto",
+    margin: 15,
+    borderWidth: 1,
+    borderColor: BORDER_COLOR,
+    borderRadius: 10,
+  },
+});
