3a99c74db57154584f35e2194ab9f1766eb888eb
unknown <ashikrobi@gmail.com>
2021-06-04 10:41:57 +0600

    add variables and functions

--- components/ReloadIcon.js
+++ components/ReloadIcon.js
@@ -1,16 +1,17 @@
 import React from "react";
 import {View, Platform, StyleSheet} from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import {Ionicons} from "@expo/vector-icons";
+import {colors} from "../Utils/brandColors";
 
 export default function ReloadIcon() {
   const reloadIconName = Platform.OS === "ios" ? "ios-refresh" : "md-refresh";
   return (
     <View style={styles.reloadIcon}>
       <Ionicons
-        onPress={ } //add load function after it is created
-        name={reloadIconName }
+        onPress={load}
+        name={reloadIconName}
         size={24}
-        color={} //add primary color
+        color={colors.PRIMARY_COLOR}
       />
     </View>
   );
