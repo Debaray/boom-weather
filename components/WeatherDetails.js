356c9a23909d81e4938d1152d710b9f38d0e60be
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-06-04 11:37:17 +0800

    import utils colors and distructure colors

--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { colors } from '../utils/index';
+
+const { PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR } = colors;
 
 export default function WeatherDetails() {
     return (
