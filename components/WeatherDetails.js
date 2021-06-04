b83ba22948ce5ecd18977d12c49e152913d2289c
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-06-04 12:35:24 +0800

    added design  weatherDetailsItems section

--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -73,3 +73,7 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
     },
+    weatherDetailsItems: {
+        alignItems: 'flex-end',
+        justifyContent: 'flex-end',
+    },
