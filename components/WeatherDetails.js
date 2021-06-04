7677320dba0f6f855ae6a8ff9a649bbf0e2d61af
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-06-04 11:42:33 +0800

    import icon and stylesheet

--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native';
 import { colors } from '../utils/index';
-
+import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 const { PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR } = colors;
 
 export default function WeatherDetails() {
