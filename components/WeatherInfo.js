99e3a537e56f9a083634c2da54474730717495e2
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-06-04 10:16:47 +0800

    import colors utis and distructure colors

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { colors } from '../utils/index';
+
+const { PRIMARY_COLOR, SECONDARY_COLOR } = colors;
 
 export default function WeatherInfo() {
     return (
